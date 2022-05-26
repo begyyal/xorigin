7d53657c73f88bbda7db80f05b518b992b611e93
begyyal <begyyal@gmail.com>
2022-05-27 07:15:07 +0900

    wip

--- .github/workflows/sh/check_if_need_test.sh
+++ .github/workflows/sh/check_if_need_test.sh
@@ -9,5 +9,20 @@ function end(){
   exit $1
 }
 
+bf=$1
+af=$2
 
+git diff $bf..$af --name-only | 
+sort > ${tmp}diff
+cat > ${tmp}ig_pattern << EOF
+README.md$
+EOF
+
+cat ${tmp}ig_pattern |
+while read ig; do
+  cat ${tmp}diff |
+  awk "\$0 ~ /$ig/ {print \$0}" 
+done | sort | uniq > ${tmp}ignored
+
+diff ${tmp}diff ${tmp}ignored 2>/dev/null >&2 || echo true
 end 0
